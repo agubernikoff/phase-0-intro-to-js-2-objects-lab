bc1101d6d2d4034f2212ceace0b7efe40f4e46c3
alexander gubernikoff <agubernikoff@gmail.com>
2022-02-01 19:42:59 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 // Write your solution in this file!
 const employee = {
-    name:"john smith",
-    streetAddress:"1 first st",
+  name: "john smith",
+  streetAddress: "1 first st",
 };
-function updateEmployeeWithKeyAndValue(object,key,value){
-    return {...employee,
-    [key]: value,
-   };
+function updateEmployeeWithKeyAndValue(object, key, value) {
+  return { ...employee, [key]: value };
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,object){
-    employee.name = "Sam";
-    employee.streetAddress = "12 Broadway";
-    return employee;
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, object) {
+  employee.name = "Sam";
+  employee.streetAddress = "12 Broadway";
+  return employee;
 }
-function deleteFromEmployeeByKey(employee,key){
-    const newEmployee ={
-        ...employee,
-    }
-    delete newEmployee.name
-    return newEmployee
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = {
+    ...employee,
+  };
+  delete newEmployee.name;
+  return newEmployee;
 }
-function destructivelyDeleteFromEmployeeByKey(employee,key){
-    delete employee.name;
-    return employee;
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee.name;
+  return employee;
 }
